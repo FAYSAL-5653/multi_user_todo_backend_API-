949c8855f4e905b53f462ff3a0ec35fbe255fc28
Faysal Sampod <faysal.sampod45@gmail.com>
2023-07-23 16:02:35 +0600

    first commit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,9 +2,11 @@
 namespace App\Http\Controllers;
 
 use App\Helper\JWTToken;
+use App\Mail\OTPmail;
 use App\Models\User;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class UserController extends Controller
 {
@@ -54,6 +56,31 @@ class UserController extends Controller
 
         }
 
+    }
+
+    public function SendOTPCode(Request $request)
+    {
+
+        $email = $request->input('email');
+        $otp = rand(1000, 9999);
+        $count = User::where('email', '=', $email)->count();
+
+        if ($count == 1) {
+            // OTP Email Address
+            Mail::to($email)->send(new OTPMail($otp));
+            // OTO Code Table Update
+            User::where('email', '=', $email)->update(['otp' => $otp]);
+
+            return response()->json([
+                'status' => 'success',
+                'message' => '4 Digit OTP Code has been send to your email !',
+            ]);
+        } else {
+            return response()->json([
+                'status' => 'failed',
+                'message' => 'unauthorized',
+            ]);
+        }
     }
 
 }
